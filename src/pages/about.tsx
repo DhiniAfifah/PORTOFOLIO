49feffabffb53e0c41404279be6274813c6cace5
DhiniAfifah25 <afifahdhini25@gmail.com>
2024-12-02 21:15:50 +0700

    feat: update kecil

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -59,68 +59,70 @@ const About = () => {
         {/* END BAGIAN About */}
 
         {/* START BAGIAN Who Am I */}
-        <Stack
-          direction={{ base: "column", md: "row" }}
-          gap={{ base: 8, md: 16 }}
-          width="100%"
-          px={{ base: 6, md: 20 }}
-          alignItems="flex-start"
-          justifyContent="flex-start"
-          mt={{ base: "40px", md: "80px" }} // Sesuaikan margin untuk perangkat yang lebih besar
-          textAlign="center" // Teks di tengah untuk layar kecil
-        >
-          {/* Teks "Who am I?" */}
-          <Text
-            fontSize={{ base: "35px", md: "50px", lg: "60px" }}
-            fontWeight="medium"
-            color="black"
-            whiteSpace="normal"
+        <Stack w={"100vw"}>
+          <Stack
+            direction={{ base: "column", md: "row" }}
+            gap={{ base: 8, md: 16 }}
+            // width="100%"
+            px={{ base: 6, md: 20 }}
+            // alignItems="flex-start"
+            // justifyContent="flex-start"
+            mt={{ base: "40px", md: "80px" }} // Sesuaikan margin untuk perangkat yang lebih besar
+            textAlign="center" // Teks di tengah untuk layar kecil
+          >
+            {/* Teks "Who am I?" */}
+            <Text
+              fontSize={{ base: "35px", md: "50px", lg: "60px" }}
+              fontWeight="medium"
+              color="black"
+              whiteSpace="normal"
+              textAlign="center"
+              mt={{ base: "20px", md: "100px" }} // Mengatur margin atas untuk perangkat mobile dan desktop
+            >
+              Who am I?
+            </Text>
+          </Stack>
+
+          {/* Deskripsi */}
+          <Stack
+            direction="column" // Menyusun elemen secara vertikal
+            gap={10} // Memberikan jarak antar elemen
+            // width="100%"
+            px={{ base: 6, md: 16 }}
+            alignItems="center"
+            justifyContent="center"
+            // mt={{ base: "40px", md: "-80px" }}
             textAlign="center"
-            mt={{ base: "20px", md: "100px" }} // Mengatur margin atas untuk perangkat mobile dan desktop
           >
-            Who am I?
-          </Text>
-        </Stack>
+            <Text
+              fontSize={{ base: "18px", md: "25px", lg: "30px" }}
+              fontWeight="regular"
+              color="black"
+              textAlign={{ base: "center", md: "left" }}
+              maxWidth={{ base: "100%", md: "60%" }}
+            >
+              Hello, I’m Dhini Afifah. Lorem ipsum dolor sit amet, consectetur
+              adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat.
+            </Text>
 
-        {/* Deskripsi */}
-        <Stack
-          direction="column" // Menyusun elemen secara vertikal
-          gap={10} // Memberikan jarak antar elemen
-          width="100%"
-          px={{ base: 6, md: 16 }}
-          alignItems="center"
-          justifyContent="center"
-          mt={{ base: "40px", md: "-80px" }}
-          textAlign="center"
-        >
-          <Text
-            fontSize={{ base: "18px", md: "25px", lg: "30px" }}
-            fontWeight="regular"
-            color="black"
-            textAlign={{ base: "center", md: "left" }}
-            maxWidth={{ base: "100%", md: "60%" }}
-          >
-            Hello, I’m Dhini Afifah. Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat.
-          </Text>
-
-          <Text
-            fontSize={{ base: "18px", md: "25px", lg: "30px" }}
-            fontWeight="regular"
-            color="black"
-            textAlign={{ base: "center", md: "left" }}
-            maxWidth={{ base: "100%", md: "60%" }}
-          >
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </Text>
+            <Text
+              fontSize={{ base: "18px", md: "25px", lg: "30px" }}
+              fontWeight="regular"
+              color="black"
+              textAlign={{ base: "center", md: "left" }}
+              maxWidth={{ base: "100%", md: "60%" }}
+            >
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum.
+            </Text>
+          </Stack>
+          {/* END BAGIAN Who Am I */}
         </Stack>
-        {/* END BAGIAN Who Am I */}
       </Stack>
       {/* END Background */}
       <Footer />
